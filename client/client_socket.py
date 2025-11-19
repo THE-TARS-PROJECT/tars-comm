844b1e0068d8216b4151ea0b46b1836f85bb5d7c
Raghav67816 <kumaraghav079@gmail.com>
2025-11-19 16:04:16 +0530

    jeepney poor documentation, trying different impl of dbus

--- client/client_socket.py
+++ client/client_socket.py
@@ -1,9 +1,6 @@
 # implementation with dbus - systemd service
 from asyncio import run
 from socketio.async_client import AsyncClient
-from jeepney import DBusAddress, new_method_call
-from jeepney.bus_messages import message_bus
-from jeepney.io.asyncio import open_dbus_connection
 
 from client_auth import Authenticator
 
@@ -16,24 +13,10 @@ class ClientService(AsyncClient):
     def __init__(self):
         super(ClientService, self).__init__()
 
-        self.dbus_session = DBusAddress(
-            object_path="/com/cooper/tars/com",
-            bus_name="com.cooper.tars",
-            interface="com.cooper.tars.Interface"
-        )
-    
         self.auth = Authenticator()
         self.config = self.auth.read_config()
 
         self.conn = None
-
-    async def start_dbus(self):
-        self.conn = await open_dbus_connection("SESSION")
-        message_bus.RequestName("com.cooper.tars")
-
-        while True:
-            msg = await self.conn.receive()
-            header = msg.header
 
     async def connect_to_server(self):
         try:
